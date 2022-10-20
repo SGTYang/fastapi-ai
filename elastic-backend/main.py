a7ae96c2c13ee3db867605ed189f1ab00d7d650c
SGTYang <wogh9504@naver.com>
2022-10-20 10:40:47 +0900

    작업전 commit

--- elastic-backend/main.py
+++ elastic-backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI, Request
 import requests, os, json
-from typing import Union, Dict
+from typing import Union, Dict, List
 
 ELASTIC_HOST = os.environ.get("ELASTIC_HOST", "192.168.0.212")
 ELASTIC_PORT = os.environ.get("ELASTIC_PORT", "9200")
@@ -23,12 +23,21 @@ def makeMatchQuery(field: str, **option: Dict[str, str]):
         }
     }
 
-@app.post("/elastic/{search}")
-async def getImagePath(field: str, option:Dict[str, str]):
+@app.post("/AI/{model_name}/")
+async def trainModel(model_name: str, images: List[str]):
+    print("Model Name:", model_name)
+    for image in images:
+        print(image)
+    return
+
+@app.post("/elastic/{field}/")
+async def getImagePath(field: str, option:Dict[str, str] = {"query":"NA"}):
     headers = {'Content-Type': 'application/json'}
     query_res = makeMatchQuery(field, **option)
-    return requests.get(
+    response = requests.get(
         f"http://{ELASTIC_HOST}:{ELASTIC_PORT}/{INDEX}/_search?_source_includes=path",
         headers=headers,
         data=json.dumps(query_res)
     ).json()
+    print([i["_source"]["path"] for i in response["hits"]["hits"]])
+    return response
--- elastic-backend/test_main.py
+++ elastic-backend/test_main.py
@@ -2,7 +2,7 @@ import pytest
 import main
 from httpx import AsyncClient
 
-matchQuery1 = {
+test_matchQuery1 = {
         "query":{
             "match":{
                 "image_type":{
@@ -12,7 +12,7 @@ matchQuery1 = {
         }
     }
 
-matchQuery2 = {
+test_matchQuery2 = {
         "query":{
             "match":{
                 "image_type":{
@@ -24,8 +24,8 @@ matchQuery2 = {
     }
 
 def test_makeMatchQuery():
-    assert main.makeMatchQuery("image_type", query = "NA") == matchQuery1
-    assert main.makeMatchQuery("image_type", query = "NA", operator = "OR") == matchQuery2
+    assert main.makeMatchQuery("image_type", query = "NA") == test_matchQuery1
+    assert main.makeMatchQuery("image_type", query = "NA", operator = "OR") == test_matchQuery2
 
 @pytest.mark.asyncio
 async def test_root():
@@ -33,3 +33,9 @@ async def test_root():
         response = await ac.get("/")
         assert response.status_code >= 200
         assert response.json() == {"message": "Hello World"}
+
+@pytest.mark.asyncio
+async def test_getImagePath():
+    async with AsyncClient(app = main.app, base_url="http://127.0.0.1:8000") as ac:
+        response = await ac.post("/elastic/image_type")
+        assert response.status_code >= 200
